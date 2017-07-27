c33ec35df180b632df25c44d64b310621dfacfe6
Han Lazarus <han.lazarus@gmail.com>
2017-07-26 23:34:11 -0700

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -2,11 +2,11 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from lists.views import home_page
+from lists.models import Item
 from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
-
 
 	def test_home_page_returns_correct_html(self):
 		response = self.client.get('/')
@@ -14,16 +14,7 @@ class HomePageTest(TestCase):
 		self.assertTrue(html.startswith('<html>'))
 		self.assertIn('<title>To-Do Lists</title>', html)
 		self.assertTrue(html.endswith('</html>'))
-		# django test - same as function below
 		self.assertTemplateUsed(response, 'home.html')
-
-	# this version of test doesn't work with csrf token
-	# def test_home_page_returns_correct_html_page(self):
-	# 	request = HttpRequest()
-	# 	response = home_page(request)
-	# 	html = response.content.decode('utf8')
-	# 	expected_html = render_to_string('home.html')
-	# 	self.assertEqual(html, expected_html)
 
 
 	def test_uses_home_template(self):
@@ -36,3 +27,21 @@ class HomePageTest(TestCase):
 		self.assertIn('A new list item', response.content.decode())
 		self.assertTemplateUsed(response, 'home.html')
 
+
+class ItemModelTest(TestCase):
+
+
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first ever list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'The second item.'
+		second_item.save()
+
+		saved_items = Items.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		self.assertEqual(first_item, saved_items[0])
+		self.assertEqual(second_item, saved_items[1])
