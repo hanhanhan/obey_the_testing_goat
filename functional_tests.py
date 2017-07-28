e0ecf368cd2aead131418c0a98c102e7b841b906
Han Lazarus <han.lazarus@gmail.com>
2017-07-28 13:47:15 -0700

    remove unittest from imports, add second peacock entry check

--- functional_tests.py
+++ functional_tests.py
@@ -2,23 +2,25 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
+
 
 class NewVisitorTest(LiveServerTestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text, [row.text for row in rows])
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		
+
 	def tearDown(self):
 		self.browser.quit()
 
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get(self.liveserverurl)
+		self.browser.get(self.live_server_url)
 		
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -38,8 +40,8 @@ class NewVisitorTest(LiveServerTestCase):
 		
 		time.sleep(5)
 		self.check_for_row_in_list_table('1. Buy peacock feathers')
-		# table = self.browser.find_element_by_id('id_list_table')
-		# rows = self.browser.find_elements_by_tag_name('tr')
+		self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
+
 		# self.assertIn('1. Buy peacock feathers',  [row.text for row in rows])
 		self.fail('Finish the test!')
 
